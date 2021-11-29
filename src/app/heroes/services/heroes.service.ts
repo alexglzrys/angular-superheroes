2c895fe332e8c6c71c0193b5a1fb38054eb577d3
Alejandro González Reyes <alexglzrys@gmail.com>
2021-11-28 20:27:40 -0600

    agregar variables de entorno

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Heroe } from '../interfaces/heroe';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
+
+  // Recuperar información de nuestra variable de entorno (siempre se importa la de desarrollo, cuando se empaqueta la aplicación, Angular de forma automática inyecta la de producción)
+  private baseUrl: string = environment.baseUrl
 
   // Inyectar el servicio de http de Angular
   constructor(private http: HttpClient) { }
@@ -15,11 +19,11 @@ export class HeroesService {
   getHeroes(): Observable<Heroe[]> {
     // Comunicarse a un endpoint y recuperar el listado de heroes desde el backend
     // Indicar a TS que la data devuelta es un arreglo de tipo Heroes
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
   }
 
   // El método devuelve un Observable de tipo Heroe
   getHeroe(id: string): Observable<Heroe> {
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
   }
 }
