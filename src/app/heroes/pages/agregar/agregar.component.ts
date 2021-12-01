52b5bdb58ffd7c06ce5b342c3159d69d15e4bb12
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-01 17:05:58 -0600

    diseño interfaz agregar super héroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroe';
 
 @Component({
   selector: 'app-agregar',
@@ -6,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar.component.css']
 })
 export class AgregarComponent implements OnInit {
+
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ]
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_image: ''
+  }
 
   constructor() { }
 
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -13,6 +13,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from '@angular/material/select';
 
 
 @NgModule({
@@ -35,6 +36,7 @@ import {MatInputModule} from "@angular/material/input";
     MatAutocompleteModule,
     MatFormFieldModule,
     MatInputModule,
+    MatSelectModule,
   ]
 })
 export class MaterialModule { }
