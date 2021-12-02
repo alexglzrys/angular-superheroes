4143929132ee6ea315bf402e1e3e8818b2f9ee7e
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-02 12:57:00 -0600

    mostrar info del usuario logeado

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth';
 
@@ -10,11 +11,24 @@ import { Auth } from '../interfaces/auth';
 export class AuthService {
 
   private BASE_URL = environment.baseUrl;
+  private _auth: Auth | undefined;
+
+  // Para compartir data almacenada en propiedades de nuestro service. se recomienda que hacer uso de getters y retornar la info (decorada, o protegerla sobre todo si se trata de arrays u objetos) ya que estos se pasan por referncia (destructurar es la opción)
+  // que se desea compartir con los clientes de este servicio
+  // Se puede hacer uso de metodos, pero todo depende de la forma o estilo de programación
+  get auth(): Auth {
+    return {...this._auth}
+  }
 
   constructor(private http: HttpClient) { }
 
   login(): Observable<Auth> {
     // El backend retorna un objeto vacio si no existe el usuario
-    return this.http.get<Auth>(`${this.BASE_URL}/usuarios/12`)
+    // El operator tap RXjs permite realizar tareas secundarias entes de enviar la respuesta que retorna la suscripcipon
+
+    // Me interesa guardar los datos del usuario en mi variable privada.
+    return this.http.get<Auth>(`${this.BASE_URL}/usuarios/1`).pipe(
+      tap(res => this._auth = {...res})
+    )
   }
 }
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Auth } from 'src/app/auth/interfaces/auth';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,14 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  // Getter que recupera del servicio los datos del usuairo logeado
+  // El objetivo es llamarlo desde la vista para mostrar el nombre del usuario {{ auth.usuario }}
+  get auth(): Auth {
+    return this.authService.auth;
+  }
+
+  constructor(private router: Router,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
   }
