15d785231401051632c416a41a54aecb9df919cb
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-02 12:22:26 -0600

    pantalla de login básica

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AuthRoutingModule } from './auth-routing.module';
     CommonModule,
     // Registrar el modulo de rutas hijas asociadas con este módulo
     AuthRoutingModule,
+    MaterialModule,
   ]
 })
 export class AuthModule { }
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    // Hacer cierta lógica de negocio antes de redireccionar
+
+    this.router.navigate(['/auth/login'])
   }
 
 }
